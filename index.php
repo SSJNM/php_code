6854f32da64b440e8d471d73587d73a04177a518
SSJNM <64457549+SSJNM@users.noreply.github.com>
2020-06-14 17:32:20 +0530

    second commit

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 <body bgcolor="orange">
 Welcome to my Cloud Site
 </br>
-<img src="image1url" alt="Here it Comes one of the Songs my Brother Likes" width="1000" height="700">
+<img src="image1url/image1.jpg/" alt="Here it Comes one of the Songs my Brother Likes" width="800" height="500">
 <pre>
 </br>
+This is one of songs my brother likes  
 Some information data about Cloud instance 
 <?php
 print `ifconfig`;

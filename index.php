2f1e929ba2d594d0049e94e9770943d7738766ba
SSJNM <64457549+SSJNM@users.noreply.github.com>
2020-07-31 10:06:42 +0530

    updated code

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <body bgcolor="orange">
 Welcome to my Cloud Site
 </br>
-<img src="https://myimage/hello width="800" height="500">
+<img src="https://cloudfrontip/imagename width="800" height="500">
 <pre>
 </br>
 This is one of songs my brother likes  
